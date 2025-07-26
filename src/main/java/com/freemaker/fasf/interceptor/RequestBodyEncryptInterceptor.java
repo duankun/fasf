e1c4b4118814beb0f7bf43b4e276013cd7179fa9
duankun <812650934@qq.com>
2025-07-26 17:20:28 +0800

    request body interceptor init

---
+++ src/main/java/com/freemaker/fasf/interceptor/RequestBodyEncryptInterceptor.java
@@ -0,0 +1,14 @@
+package com.freemaker.fasf.interceptor;
+
+import com.freemaker.fasf.http.HttpRequest;
+
+/**
+ * @author duankun
+ * @since 2025/7/26
+ */
+public class RequestBodyEncryptInterceptor implements RequestInterceptor{
+    @Override
+    public void intercept(HttpRequest request) {
+
+    }
+}
