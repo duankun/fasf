c197ed47068700be38c59180af12852fa45958ea
duankun <812650934@qq.com>
2025-08-12 10:46:18 +0800

    remoter method level interceptor support

--- src/main/java/com/freemaker/fasf/annotation/Request.java
+++ src/main/java/com/freemaker/fasf/annotation/Request.java
@@ -9,7 +9,7 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
-public @interface RequestLine {
+public @interface Request {
     String path() default "";
     HttpMethod method() default HttpMethod.POST;
 }
