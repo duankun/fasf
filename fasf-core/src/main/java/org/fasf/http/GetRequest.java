6c75648e582ebcbab0cf463abff352cd95aea5ef
duankun <812650934@qq.com>
2025-08-30 20:02:20 +0800

    look better

--- fasf-core/src/main/java/org/fasf/http/GetRequest.java
+++ fasf-core/src/main/java/org/fasf/http/GetRequest.java
@@ -2,6 +2,7 @@ package org.fasf.http;
 
 
 import org.fasf.util.JSON;
+import org.springframework.util.CollectionUtils;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -13,12 +14,16 @@ public class GetRequest extends HttpRequest {
     public GetRequest(String url, Map<String, String> queryParameters) {
         super(url);
         this.originQueryParameters.putAll(queryParameters);
-        this.queryParameters = queryParameters;
+        this.queryParameters = CollectionUtils.isEmpty(queryParameters) ? new HashMap<>() : queryParameters;
     }
 
 
     public Map<String, String> getQueryParameters() {
         return queryParameters;
+    }
+
+    public void addParameter(String key, String value) {
+        queryParameters.put(key, value);
     }
 
     @Override
